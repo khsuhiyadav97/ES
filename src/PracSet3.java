41436715423fdade976359c8492f15ad20908789
khushiyadav97 <ky622933@gmail.com>
2026-09-25 22:31:58 +0530

    Looping

---
+++ src/PracSet3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PracSet2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n:");
+        int n = sc.nextInt();
+        int a = 123;
+
+
+    }
+}
